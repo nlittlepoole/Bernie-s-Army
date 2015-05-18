16823073564d513308a2b7f92c356f7c3124bfef
Niger Little-Poole <nl2418@columbia.edu>
2015-05-18 18:18:41 +0000

    removed unnecessary url routes

--- OWL/urls.py
+++ OWL/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     url(r'home', views.index, name='index'),
     url(r'signup', views.signup, name='signup'),
     url(r'activate', views.activate, name='activate'),
-    url(r'command', views.command, name='command'),
-    url(r'send', views.send, name='send'),
 ]
